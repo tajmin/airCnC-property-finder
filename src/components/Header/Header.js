75caea816430457ca98f9012282d5d0d3681913a
tajmin <tajminur.aiub@gmail.com>
2021-09-26 00:38:05 +0600

    initialized react

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>RealFast</h1>
+            <h4>Real Estate Finder</h4>
+        </div>
+    );
+};
+
+export default Header;
