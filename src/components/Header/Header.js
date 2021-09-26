4c9ac7ae2991f2aa4647ccc7df8b070a954376e2
tajmin <tajminur.aiub@gmail.com>
2021-09-26 11:32:53 +0600

    icons added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
-            <h1>RealFast</h1>
+            <h1>AirC&C</h1>
             <h4>Real Estate Finder</h4>
             <p><strong>Start Buying Houses Now From Our Listings</strong></p>
         </div>
--- src/components/House/House.js
+++ src/components/House/House.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './House.css'
+import { faCartPlus, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const House = (props) => {
     const { price, thumb, bed, bath, listedBy, address, yearBuilt } = props.house;
@@ -9,12 +11,12 @@ const House = (props) => {
             <img className="card-image" src={thumb} alt="" />
             <div className="card-body">
                 <h5 className="title">${price.toLocaleString()}</h5>
-                <h6 className="secondary-title">{address}</h6>
+                <h6 className="secondary-title"><FontAwesomeIcon icon={faMapMarkerAlt} /> {address}</h6>
                 <p>{bed} Bed | {bath} Bath</p>
                 <p className="card-text"><small>Property listed by {listedBy}</small></p>
                 <p>Built on {yearBuilt}</p>
             </div>
-            {/* <button onClick={() => props.handleDetails(meal)}><FontAwesomeIcon icon={faInfo} /> Details</button> */}
+            <button onClick={() => props.handleDetails()}><FontAwesomeIcon icon={faCartPlus} /> Add to Cart</button>
         </div>
     );
 };
